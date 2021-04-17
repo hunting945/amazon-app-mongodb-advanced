a97763345a0d5a30b996796d74b9bc506b94ddcb
hunterlai <hunter.lai.corp@outlook.com>
2021-04-17 17:05:26 +0800

    first commit

---
+++ backend/data.js
@@ -0,0 +1,70 @@
+export default {
+    products: [
+        {
+            _id: '1',
+            name: 'Regular Fit Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'BOSS',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6
+        },
+        {
+            _id: '2',
+            name: 'Slim Fit Shirt',
+            category: 'Shirts',
+            image: '/images/d2.jpg',
+            price: 50,
+            brand: 'BOSS',
+            rating: 4.2,
+            numReviews: 5,
+            countInStock: 8
+        },
+        {
+            _id: '3',
+            name: 'Relaxed Fit Shirt',
+            category: 'Shirts',
+            image: '/images/d3.jpg',
+            price: 70,
+            brand: 'BOSS',
+            rating: 4.5,
+            numReviews: 8,
+            countInStock: 6
+        },
+        {
+            _id: '4',
+            name: 'Regular Fit Pants',
+            category: 'Pants',
+            image: '/images/d4.jpg',
+            price: 70,
+            brand: 'BOSS',
+            rating: 4.0,
+            numReviews: 8,
+            countInStock: 6
+        },
+        {
+            _id: '5',
+            name: 'Slim Fit Pants',
+            category: 'Pants',
+            image: '/images/d5.jpg',
+            price: 70,
+            brand: 'BOSS',
+            rating: 3.8,
+            numReviews: 8,
+            countInStock: 5
+        },
+        {
+            _id: '6',
+            name: 'Relaxed Fit Pants',
+            category: 'Pants',
+            image: '/images/d6.jpg',
+            price: 70,
+            brand: 'BOSS',
+            rating: 3.3,
+            numReviews: 8,
+            countInStock: 3
+        },
+    ]
+}
