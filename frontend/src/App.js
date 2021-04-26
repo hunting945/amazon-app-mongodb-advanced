1391807f0273a30143022ede4f54dd10e0a262f5
hunterlai <hunter.lai.corp@outlook.com>
2021-04-27 03:18:25 +0800

    add products getting from mongodb

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import ProductScreen from './Screens/ProductScreen';
 import SigninScreen from './Screens/SigninScreen';
 import { useSelector } from 'react-redux';
 import RegisterScreen from './Screens/RegisterScreen';
+import ProductsScreen from './Screens/ProductsScreen';
 
 function App() {
 
@@ -53,6 +54,7 @@ function App() {
         </aside>
         <main className="main">
           <div className="content">
+            <Route path="/products" component={ProductsScreen} />
             <Route path="/signin" component={SigninScreen} />
             <Route path="/register" component={RegisterScreen} />
             <Route path="/product/:id" component={ProductScreen} />
