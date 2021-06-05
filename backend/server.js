47841698e1c1cd4c3645c8cb90f8ab880e049a24
hunterlai <hunter.lai.corp@outlook.com>
2021-06-06 01:21:49 +0800

    mod heroku setting

--- backend/server.js
+++ backend/server.js
@@ -27,12 +27,12 @@ app.get("/api/config/paypal", (req, res) => {
   res.send(config.PAYPAL_CLIENT_ID);
 })
 
-const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-);
+// const __dirname = path.resolve();
+// app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+// app.use(express.static(path.join(__dirname, '/frontend/build')));
+// app.get('*', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+// );
 
 // app.get("/api/products/:id", (req, res) => {
 //   const productId = req.params.id;
